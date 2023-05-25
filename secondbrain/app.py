409882d38934873904a461383ae7a9b7225c76c8
Kumar Saksham <everydaycodings@gmail.com>
2023-05-25 23:13:01 +0530

    base code for source embeddings

--- secondbrain/app.py
+++ secondbrain/app.py
@@ -11,6 +11,8 @@ from langchain.vectorstores import SupabaseVectorStore
 from supabase import Client, create_client
 from explorer import view_document
 from stats import get_usage_today
+from helpers.source_embedding import load_pdf, load_embedding_model, retriver, source_docs
+
 
 supabase_url = st.secrets.supabase_url
 supabase_key = st.secrets.supabase_service_key
@@ -81,6 +83,30 @@ if user_choice == "Source Embedding":
         "Select Chunk Size", 100, 1000, st.session_state['chunk_size'], 50)
     st.session_state['chunk_overlap'] = st.sidebar.slider(
         "Select Chunk Overlap", 0, 500, st.session_state['chunk_overlap'], 10)
+    
+    col1, col2 = st.columns(2)
+
+    with col1:
+        pdf_file = st.file_uploader("Upload The PDF: ")
+        search_arg = st.number_input("Number of Closest Relevent Search: ", min_value=1, step=1, value=3)
+    with col2:
+        model_name = st.selectbox(label="Select Your Source Embedding Model: ", options=["hkunlp/instructor-xl"])
+        st.text(" ")
+        st.text(" ")
+        device = st.selectbox(label="Select Your Device: ", options=["cuda", "cpu"])
+
+    
+    if st.button("Load Model"):
+        with st.spinner("Model Loading..."):
+            pdf_pages = load_pdf(pdf_file)
+            embeddingings, condition = load_embedding_model(model_name, device)
+            retrive = retriver(embeddingings, pdf_pages, search_arg)
+        
+        prompt = st.text_input("Enter Your Prompt: ", value="Explain Proof-Of-Work?")
+        docs_result =source_docs(retrive, prompt)
+        st.text(docs_result)
+
+
     
 
 if user_choice == 'Add Knowledge':
---
+++ secondbrain/helpers/source_embedding.py
@@ -0,0 +1,46 @@
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+import streamlit as st
+import tempfile, os
+
+
+def load_pdf(pdf_file):
+
+    if pdf_file != None:
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+
+            file_path = file_path = os.path.join(temp_dir, pdf_file.name)
+            with open(file_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+
+            loader = PyPDFLoader(file_path)
+            pages = loader.load_and_split()
+
+            return pages
+
+
+def load_embedding_model(model_name, device):
+
+    instructor_embeddings = HuggingFaceInstructEmbeddings(model_name=model_name,
+                                                      model_kwargs={"device": device})
+    
+    return instructor_embeddings, True
+
+
+def retriver(embeddings, pages, num):
+    db_instructEmbedd = FAISS.from_documents(pages, embeddings)
+    retriever_sec = db_instructEmbedd.as_retriever(search_kwargs={"k": num})
+
+    return retriever_sec
+
+
+def source_docs(retriever, prompt):
+    
+    docs_result = retriever.get_relevant_documents(prompt)
+    return docs_result
